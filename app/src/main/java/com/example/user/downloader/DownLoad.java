1d7bcde64b0176a03ca0d13f2c5bafe8014b0a03
chihirons <38895134+chihirons@users.noreply.github.com>
2018-06-04 14:36:17 +0900

    Initial commit

---
+++ app/src/main/java/com/example/user/downloader/DownLoad.java
@@ -0,0 +1,108 @@
+package com.example.user.downloader;
+
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class DownLoad extends AppCompatActivity {
+
+    Button Clear;
+    EditText Texts;
+    Button Starts;
+    ImageView Views;
+    DownLoadAsyncTask task;
+
+    Toast no;
+    Toast ok;
+
+    String url;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_down_load);
+
+            no = Toast.makeText(this, "画像取得に失敗", Toast.LENGTH_LONG);
+            ok = Toast.makeText(this, "ダウンロードが完了しました", Toast.LENGTH_LONG);
+
+            //紐づけ
+            Texts = findViewById(R.id.texts);
+            Starts = findViewById(R.id.starts);
+            Views = findViewById(R.id.views);
+            Clear = findViewById(R.id.clear);
+
+
+            //ダウンロード開始ボタンタップ時
+            Starts.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    url = Texts.getText().toString();
+
+                    if (url.length() != 0) {
+                        //EditTextに文字が入っている場合
+                        task = new DownLoadAsyncTask();
+                        //Lisntenerを設定
+                        task.setListener(createListener());
+                        task.execute(url);
+                    } else {
+                        no.show();
+
+                    }
+                }
+            });
+
+
+            //画像とテキストのクリアー
+            Clear.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Texts.setText("");
+                    Views.setImageDrawable(null);
+                }
+            });
+        }
+
+//    @Override
+//    protected void onDestroy() {
+//        task.setListener(null);
+//        super.onDestroy();
+//    }
+
+        private DownLoadAsyncTask.Listener createListener () {
+            return new DownLoadAsyncTask.Listener() {
+                @Override
+                public void onSuccess(Bitmap bpm) {
+                    Views.setImageBitmap(bpm);
+                    if (bpm == null) {
+                        no.show();
+                    } else {
+                        ok.show();
+                        setSaved();
+                    }
+                    Log.d("BPMの中身", String.valueOf(bpm));
+                }
+            };
+        }
+
+        //保存用のクラスに値渡し
+        private void setSaved () {
+            Bitmap bitmapImage = ((BitmapDrawable) Views.getDrawable()).getBitmap();
+            Material material = new Material(this);
+
+            try {
+                material.drectoryM(bitmapImage);
+                Toast.makeText(DownLoad.this, "保存できました", Toast.LENGTH_SHORT).show();
+
+            } catch (Error e) {
+                Log.e("setSave", "Error:" + e);
+                Toast.makeText(DownLoad.this, "保存できません", Toast.LENGTH_SHORT).show();
+            }
+        }
+}
---
+++ app/src/main/java/com/example/user/downloader/Material.java
@@ -0,0 +1,82 @@
+package com.example.user.downloader;
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+public class Material {
+
+    private Context mContext;
+
+    public Material(Context context){
+        this.mContext = context;
+    }
+
+    //ファイルネーム生成
+    private String getFileName(){
+        Date mDate = new Date();
+        SimpleDateFormat fileNameFormat = new SimpleDateFormat("yyyyMMdd", Locale.ENGLISH);
+        String saveName = fileNameFormat.format(mDate);
+        return saveName;
+    }
+
+    //ディレクトリ取得とファイル生成の処理
+    public void drectoryM(Bitmap bmp){
+        String savePath = Environment.getExternalStorageDirectory().getPath() + "To";
+
+        /*
+            指定されたパス名文字列を抽象パス名に変換して、新しいFileインスタンスを作成します。
+            抽象パス...OSに依存しないパスのこと
+        */
+        File file = new File(savePath);
+
+        //exists()...ファイルが存在するかの確認
+        if (!file.exists()){
+            //今回はファイルが存在しなかった場合、ファイル作成
+            file.mkdir();
+        }
+
+        //絶対パスを生成
+        String imagePath = savePath + '/' + "IMG_" + getFileName() + ".jpg"; //ファイル名取得
+
+        try{
+            FileOutputStream fos = new FileOutputStream(imagePath, true);
+            //指定された大きさの圧縮された画像が保存されっる
+            bmp.compress(Bitmap.CompressFormat.JPEG,100,fos);
+            fos.flush(); //キャッシュ管理
+            fos.close();
+
+        } catch (Exception e) {
+            e.getMessage();
+        }finally {
+            pushGallery(getFileName());
+        }
+    }
+
+    //更新プログラム
+    private void pushGallery(String getFileName){
+        try {
+            //ContentProviderの更新
+            ContentValues values = new ContentValues();
+            values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+            values.put(MediaStore.Images.Media.TITLE, getFileName()); //ファイル名取得
+            values.put("_data", getFileName);
+
+            ContentResolver resolver = mContext.getContentResolver();
+            resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+
+        }catch (Exception e){
+            Log.e("Error", "Error :" + e);
+        }
+    }
+}
